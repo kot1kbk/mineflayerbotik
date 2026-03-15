0bf59d5f44bee2bd07f70c8cb4595f741f43ac88
kot1kbk <alinamalinka122@gmail.com>
2026-03-15 20:41:35 +0300

    Create utils.js

---
+++ telegram/utils.js
@@ -0,0 +1,38 @@
+// telegram/utils.js
+function escapeMarkdown(text) {
+    if (!text) return '';
+    return text.toString()
+    .replace(/_/g, '\\_')
+    .replace(/\*/g, '\\*')
+    .replace(/\[/g, '\\[')
+    .replace(/\]/g, '\\]')
+    .replace(/\(/g, '\\(')
+    .replace(/\)/g, '\\)')
+    .replace(/~/g, '\\~')
+    .replace(/`/g, '\\`')
+    .replace(/>/g, '\\>')
+    .replace(/#/g, '\\#')
+    .replace(/\+/g, '\\+')
+    .replace(/-/g, '\\-')
+    .replace(/=/g, '\\=')
+    .replace(/\|/g, '\\|')
+    .replace(/\{/g, '\\{')
+        .replace(/\}/g, '\\}')
+        .replace(/\./g, '\\.')
+        .replace(/!/g, '\\!');
+}
+
+function escapeHtml(text) {
+    if (!text) return '';
+    return text
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
+module.exports = {
+    escapeMarkdown,
+    escapeHtml
+};
